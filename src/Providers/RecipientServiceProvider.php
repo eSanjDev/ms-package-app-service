ab598744393f92d55b4b360c87d1e5ab395df5da
Ali Fadaei <alifadaei_123@yahoo.com>
2025-07-26 11:32:29 +0330

    feat: first comit

---
+++ src/Providers/RecipientServiceProvider.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Esanj\Recipient\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class RecipientServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+
+    }
+
+    public function boot()
+    {
+
+    }
+
+}
