5c9c7bbb3774cb926948d7f5c90503974b4893b5
Ali Fadaei <alifadaei_123@yahoo.com>
2025-08-06 13:46:57 +0330

    refactor: minor change

--- src/views/create.blade.php
+++ src/views/create.blade.php
@@ -15,50 +15,48 @@
 @endsection
 
 @section('content')
-    <div class="layout-services">
-        <h2>Add New Service</h2>
-        <form class="form-setting mt-2" action="{{route("services.store")}}" method="post">
-            @csrf
-            <div class="row ">
-                <div class="col-12">
-                    <div class="card p-6">
-                        <h3 class="mb-6">Service Info</h3>
-                        <div class="row">
-                            <div class="col-lg-4 mb-3">
-                                <div class="form-floating">
-                                    <input type="text" class="form-control" value="{{old('name')}}"
-                                           placeholder="Enter Service Name" name="name"/>
-                                    <label class="form-label">Name</label>
-                                </div>
-                                @error('name')
-                                <div class="text-danger">{{ $message }}</div> @enderror
+    <h2>Add New Service</h2>
+    <form class="form-setting mt-2" action="{{route("services.store")}}" method="post">
+        @csrf
+        <div class="row ">
+            <div class="col-12">
+                <div class="card p-6">
+                    <h3 class="mb-6">Service Info</h3>
+                    <div class="row">
+                        <div class="col-lg-4 mb-3">
+                            <div class="form-floating">
+                                <input type="text" class="form-control" value="{{old('name')}}"
+                                       placeholder="Enter Service Name" name="name"/>
+                                <label class="form-label">Name</label>
                             </div>
-                            <div class="col-lg-4 mb-3">
-                                <div class="form-floating position-relative">
-                                    <input type="text" class="form-control" value="{{old('client_id')}}"
-                                           placeholder="Enter Client Id" name="client_id" id="clientID"/>
-                                    <label class="form-label">Client ID</label>
-                                    <button class="btn btn-validate" type="button" id="validation-client">Validate</button>
-                                </div>
-                                @error('client_id')
-                                <div class="text-danger">{{ $message }}</div> @enderror
-                            </div>
-                            <div class="col-lg-4 px-2 position-relative select-box mb-4">
-                                <label class="form-label">Status</label>
-                                <select name="is_active" class="form-select select2">
-                                    <option @selected(old('is_active') == 0) value="0">Deactive</option>
-                                    <option @selected(old('is_active',1) == 1) value="1">Active</option>
-                                </select>
-                            </div>
+                            @error('name')
+                            <div class="text-danger">{{ $message }}</div> @enderror
                         </div>
-                        <div class="row">
-                            <div class="col-12">
-                                <button class="btn btn-primary mt-3">Create Service</button>
+                        <div class="col-lg-4 mb-3">
+                            <div class="form-floating position-relative">
+                                <input type="text" class="form-control" value="{{old('client_id')}}"
+                                       placeholder="Enter Client Id" name="client_id" id="clientID"/>
+                                <label class="form-label">Client ID</label>
+                                <button class="btn btn-validate" type="button" id="validation-client">Validate</button>
                             </div>
+                            @error('client_id')
+                            <div class="text-danger">{{ $message }}</div> @enderror
+                        </div>
+                        <div class="col-lg-4 px-2 position-relative select-box mb-4">
+                            <label class="form-label">Status</label>
+                            <select name="is_active" class="form-select select2">
+                                <option @selected(old('is_active') == 0) value="0">Deactive</option>
+                                <option @selected(old('is_active',1) == 1) value="1">Active</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-12">
+                            <button class="btn btn-primary mt-3">Create Service</button>
                         </div>
                     </div>
                 </div>
             </div>
-        </form>
-    </div>
+        </div>
+    </form>
 @endsection
--- src/views/edit.blade.php
+++ src/views/edit.blade.php
@@ -15,68 +15,62 @@
 @endsection
 
 @section('content')
-    <div class="layout-services">
-        <h2>Edit Service</h2>
-        <form class="form-setting mt-2" action="{{route("services.update",$service->id)}}" method="post">
-            @if(session('success'))
-                <div class="alert alert-success alert-dismissible" role="alert">
-                    {{session('success')}}
-                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                </div>
-            @endif
-            @csrf
-            @method('put')
-            <div class="row ">
-                <div class="col-12">
-                    <div class="card p-6">
-                        <h3 class="mb-6">Service Info</h3>
-                        <div class="row">
-                            <div class="col-lg-4 mb-3">
-                                <div class="form-floating">
-                                    <input type="text" class="form-control" value="{{old('name',$service->name)}}"
-                                           placeholder="Enter Service Name" name="name"/>
-                                    <label class="form-label">Name</label>
-                                </div>
-                                @error('name')
-                                <div class="text-danger">{{ $message }}</div> @enderror
+    <h2>Edit Service</h2>
+    <form class="form-setting mt-2" action="{{route("services.update",$service->id)}}" method="post">
+        @if(session('success'))
+            <div class="alert alert-success alert-dismissible" role="alert">
+                {{session('success')}}
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+        @endif
+        @csrf
+        @method('put')
+        <div class="row ">
+            <div class="col-12">
+                <div class="card p-6">
+                    <h3 class="mb-6">Service Info</h3>
+                    <div class="row">
+                        <div class="col-lg-4 mb-3">
+                            <div class="form-floating">
+                                <input type="text" class="form-control" value="{{old('name',$service->name)}}"
+                                       placeholder="Enter Service Name" name="name"/>
+                                <label class="form-label">Name</label>
                             </div>
-                            <div class="col-lg-4 mb-3">
-                                <div class="form-floating position-relative">
-                                    <input type="text" class="form-control"
-                                           value="{{old('client_id',$service->client_id)}}"
-                                           placeholder="Enter Client Id" name="client_id" id="clientID"/>
-                                    <label class="form-label">Client ID</label>
-                                    <button class="btn btn-validate" id="validation-client" type="button">Validate
-                                    </button>
-                                </div>
-                                @error('client_id')
-                                <div class="text-danger">{{ $message }}</div> @enderror
-                            </div>
-                            <div class="col-lg-4 px-2 position-relative select-box mb-4">
-                                <label class="form-label" for="validationStatus">Status</label>
-                                <select name="is_active" class="form-select select2">
-                                    <option @selected(old('is_active',$service->is_active) == 0) value="0">deactive
-                                    </option>
-                                    <option @selected(old('is_active',$service->is_active) == 1) value="1">Active
-                                    </option>
-                                </select>
-                            </div>
+                            @error('name')
+                            <div class="text-danger">{{ $message }}</div> @enderror
                         </div>
-                        <div class="row">
-                            <div class="col-12 mb-3">
-                                <button class="btn btn-primary">Update Service</button>
+                        <div class="col-lg-4 mb-3">
+                            <div class="form-floating position-relative">
+                                <input type="text" class="form-control"
+                                       value="{{old('client_id',$service->client_id)}}"
+                                       placeholder="Enter Client Id" name="client_id" id="clientID"/>
+                                <label class="form-label">Client ID</label>
+                                <button class="btn btn-validate" id="validation-client" type="button">Validate
+                                </button>
                             </div>
+                            @error('client_id')
+                            <div class="text-danger">{{ $message }}</div> @enderror
+                        </div>
+                        <div class="col-lg-4 px-2 position-relative select-box mb-4">
+                            <label class="form-label" for="validationStatus">Status</label>
+                            <select name="is_active" class="form-select select2">
+                                <option @selected(old('is_active',$service->is_active) == 0) value="0">deactive
+                                </option>
+                                <option @selected(old('is_active',$service->is_active) == 1) value="1">Active
+                                </option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-12 mb-3">
+                            <button class="btn btn-primary">Update Service</button>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="row">
-                @foreach(config('app_service.extra_fields') as $field)
-                    <div class="col-12">
-                        @include(\Illuminate\Support\Str::endsWith($field,'.blade.php')?$field:"$field.blade.php")
-                    </div>
-                @endforeach
-            </div>
-        </form>
-    </div>
+        </div>
+    </form>
+    @foreach(config('app_service.extra_fields') as $field)
+        @include($field)
+    @endforeach
 @endsection
