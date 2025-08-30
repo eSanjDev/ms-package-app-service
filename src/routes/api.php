6ff949b6ac99d7501b1ed5c221ad065c9f52f475
Ali Fadaei <alifadaei_123@yahoo.com>
2025-08-30 10:42:30 +0330

    fix: route name fixed

--- src/routes/api.php
+++ src/routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix(config('esanj.app_service.routes.api_prefix'))
     ->middleware(config('esanj.app_service.middlewares.api'))
+    ->name('api.')
     ->group(function () {
         Route::post('/services/{id}/restore', [AppServiceApiController::class, 'restore']);
         Route::get("/services/validation", [AppServiceApiController::class, 'validateClient']);
